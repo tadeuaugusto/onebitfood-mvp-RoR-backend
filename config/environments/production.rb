2417ea9271f417d1d7fddf4d3a0dfcf230bc3fbd
Tadeu Augusto Dutra Pinto <tadeuaugusto@gmail.com>
2019-05-14 04:44:17 -0300

    configured asset_host on production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,7 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  # config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
@@ -96,4 +96,10 @@ Rails.application.configure do
   ActionMailer::Base.delivery_method = :smtp
   
   config.action_mailer.default_url_options = { :host => 'https://arcane-caverns-26717.herokuapp.com' }
+
+  config.action_controller.asset_host = "http://dknv5h29u30sh.cloudfront.net"
+  config.public_file_server.enabled = true
+  config.public_file_server.headers = {
+    'Cache-Control' => 'public, max-age=31536000'
+  }
 end
